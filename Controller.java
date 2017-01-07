86aff8e032cfa726d71a87102d185bb70dea9891
randwerArbeit <marzian@innosoft.de>
2017-01-07 17:06:03 +0100

    call components in fxml

--- Controller.java
+++ Controller.java
@@ -1,17 +1,24 @@
-package verwaltungFX;
+package javaFXExample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-
+	
+	@FXML
+    private Button Peter;
+	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
     }
     
     public void handleButtonClick(){
-    	System.out.println("Ich wurde gepresst und heiﬂe Peter.");
+    	System.out.println("Ich wurde gepresst und hei√üe Peter.");
+    	Peter.setText("asdf");
     }
 }
